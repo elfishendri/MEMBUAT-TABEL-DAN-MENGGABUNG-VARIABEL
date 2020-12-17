94cb2c86493f7826b87bb37c8a0433a0ff41d6b7
Elfis Hendri, S.Pd, CH, CHt, CI, CBek, CAkp <74452353+elfishendri@users.noreply.github.com>
2020-12-17 18:56:21 +0700

    MEMBUAT-TABEL-DAN-MENGGABUNG-VARIABEL

---
+++ gabung.php
@@ -0,0 +1,35 @@
+<?php
+
+//variabel
+$kota1="jakarta";
+$kota2="pekanbaru";
+$orang1="ana";
+$orang2="ani";
+$orang3="ane";
+$orang4="ano";
+$orang5="andi";
+$orang6="ande";
+
+
+//tabel
+echo "<br>";
+
+echo "Belajar tabel dan menggabung variabel";
+
+echo "<br>";
+
+
+echo "<table cellspacing=0 width = 400 border =1>
+
+<tr bgcolor=grey>
+	<td>$kota1</td>
+	<td>$orang1,$orang2,$orang3</td>
+</tr>
+
+<tr>
+	<td>$kota2</td>
+	<td>$orang4,$orang5,$orang6</td>
+</tr>
+
+</table>";
+?>
